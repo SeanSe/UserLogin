95cc584beb297a379a1acd984c1fc9e444ce486f
Sean <ses0384@my.londonmet.ac.uk>
2016-04-23 13:37:03 +0100

    Initial commit

---
+++ app/src/main/java/com/project/sean/userlogin/LoginActivity.java
@@ -0,0 +1,119 @@
+package com.project.sean.userlogin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by Sean on 22/04/2016.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+    EditText txtUsername, txtPassword;
+
+    // User Session Manager Class
+    UserSessionManager session;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // User Session Manager
+        session = new UserSessionManager(getApplicationContext());
+
+        // get Email, Password input text
+        txtUsername = (EditText) findViewById(R.id.txtUsername);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+
+        Toast.makeText(getApplicationContext(),
+                "User Login Status: " + session.isUserLoggedIn(),
+                Toast.LENGTH_LONG).show();
+
+
+        // User Login button
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+
+        // Login button click event
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // Get username, password from EditText
+                String username = txtUsername.getText().toString();
+                String password = txtPassword.getText().toString();
+
+                // Validate if username, password is filled
+                if(username.trim().length() > 0 && password.trim().length() > 0){
+
+                    // For testing puspose username, password is checked with static data
+                    // username = admin
+                    // password = admin
+                    //In demo version it will be taken from the database
+
+                    if(username.equals("admin") && password.equals("admin")){
+
+                        // Creating user login session
+                        // Statically storing name="Sean"
+                        // and role="Manager"
+                        //Add information retrieved from database
+                        session.createUserLoginSession("Sean",
+                                "Manager");
+
+                        // Starting MainActivity
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                        // Add new Flag to start new Activity
+                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(i);
+
+                        finish();
+
+                    }else if(username.equals("emp") && password.equals("emp")) {
+                        // Creating user login session
+                        // Statically storing name="Sean"
+                        // and role="Manager"
+                        //Add information retrieved from database
+                        session.createUserLoginSession("James",
+                                "Employee");
+
+                        // Starting MainActivity
+                        Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+                        // Add new Flag to start new Activity
+                        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(i);
+
+                        finish();
+                    } else{
+
+                        // username / password doesn't match&
+                        Toast.makeText(getApplicationContext(),
+                                "Username/Password is incorrect",
+                                Toast.LENGTH_LONG).show();
+
+                    }
+                }else{
+
+                    // user didn't entered username or password
+                    Toast.makeText(getApplicationContext(),
+                            "Please enter username and password",
+                            Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/project/sean/userlogin/MainActivity.java
@@ -0,0 +1,112 @@
+package com.project.sean.userlogin;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.HashMap;
+
+/**
+ *
+ * Created by Sean on 22/04/2016.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+    // User Session Manager Class
+    UserSessionManager session;
+
+    // Button Logout
+    Button btnLogout;
+    //Button stock management
+    Button btnStockManagement;
+    //Button checkout
+    Button btnCheckout;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Session class instance
+        session = new UserSessionManager(getApplicationContext());
+
+        TextView lblName = (TextView) findViewById(R.id.lblName);
+        TextView lblEmail = (TextView) findViewById(R.id.lblEmail);
+
+        // Button logout
+        btnLogout = (Button) findViewById(R.id.btnLogout);
+        //Button stock management
+        btnStockManagement = (Button) findViewById(R.id.btnStockManagement);
+        //Button checkout
+        btnCheckout = (Button) findViewById(R.id.btnCheckout);
+
+        Toast.makeText(getApplicationContext(),
+                "User Login Status: " + session.isUserLoggedIn(),
+                Toast.LENGTH_LONG).show();
+
+
+        // Check user login (this is the important point)
+        // If User is not logged in , This will redirect user to LoginActivity
+        // and finish current activity from activity stack.
+        if (session.checkLogin())
+            finish();
+
+        // get user data from session
+        HashMap<String, String> user = session.getUserDetails();
+
+        // get name
+        String name = user.get(UserSessionManager.KEY_NAME);
+
+        // get role
+        final String role = user.get(UserSessionManager.KEY_ROLE);
+
+        // Show user data on activity
+        lblName.setText(Html.fromHtml("Name: <b>" + name + "</b>"));
+        lblEmail.setText(Html.fromHtml("Role: <b>" + role + "</b>"));
+
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                // Clear the User session data
+                // and redirect user to LoginActivity
+                session.logoutUser();
+            }
+        });
+
+        btnStockManagement.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                if(role.equals("Manager")) {
+                    Toast.makeText(getApplicationContext(), "This is stock management!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Invalid role, manager access only",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnCheckout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                if(role.equals("Manager") || role.equals("Employee") ) {
+                    Toast.makeText(getApplicationContext(), "This is Checkout!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
